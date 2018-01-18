52d6ee7e938f209a36daa59f4c0b49b6224b9a12
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-01-18 16:43:00 +0530

    Create interval.c

---
+++ interval.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{  
+int n,i,j; 
+printf("Enter the value of i:");
+scanf("%d",&i);
+printf("Enter the value of j:");
+scanf("%d",&j);
+for(n=i+1; n <= j; n++) 
+{
+if(n%2 == 1) 
+{ 
+printf("%d ",n); 
+}  
+}  
+return 0;  
+} 
