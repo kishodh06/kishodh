5ca9bf1c1100bc4f24cf4296e7a4b85e3766cd62
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-01-19 08:19:15 +0000

    Update 150.c

--- 150.c
+++ 150.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 int main()
 {
-int n1, n2, i, temp, num, rem;
-printf("Enter two numbers: ");
-scanf("%d %d", &n1, &n2);
-printf("Armstrong numbers between %d an %d are: ", n1, n2);
-for(i=n1+1; i<n2; ++i)
-{
-temp=i;
-num=0;
-while(temp!=0)
-{
-rem=(temp%10);
-num+=rem*rem*rem;
-temp/=10;
-}
-if(i==num)
-{
-printf("%d ",i);
-}
-}
-return 0;
-}
+  int n1, n2, i, t, n, c,r;
+  scanf("%d %d", &n1, &n2);
+  for(i=n1+1; i<n2; ++i)
+  {
+      t=i;
+      n=0;
+      while(t!=0)
+      {
+          r=(t%10);
+c=r*r*r;
+          n=n+c;
+          t/=10;
+      }
+      if(i==n)
+      {
+          printf("%d ",i);
+      }
+  }
+  return 0;
+} 
