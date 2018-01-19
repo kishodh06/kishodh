26707e3ceeb423c180475092da898878726a3104
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-01-19 08:36:52 +0000

    Create nad.c

---
+++ nad.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+ int n,a,d,l,sum=0;
+ printf("Enetr the value of number of values:");
+ scanf("%d",&n);
+ printf("Enter the value of number of series:");
+ scanf("%d",&a);
+ printf("Enter the value of difference:");
+ scanf("%d",&d);
+ l=(n*d)+a-1; 
+ sum=((a+l)*n)/2;
+ printf("sum is %d",sum);
+ return 0;
+}
