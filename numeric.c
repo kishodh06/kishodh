baa91dcd1df0f4a605f168473b55a36c3359e2c9
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-01-21 09:31:41 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void) 
+{
+int value,count=0,r;
+scanf("%d",&value);
+while(value>0)
+{
+r=value%10;
+if(r>=0 & r<=9)
+count++;
+value=value/10;
+}
+if(count==0)
+printf("No");
+else
+printf("Yes");
+return 0;
+}
