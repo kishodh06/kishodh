52cc92306b8ce1b1d1c62daec9868edd66c17fdc
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-01-25 16:21:14 +0530

    Create words.c

---
+++ words.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+int num;
+scanf("%d",&num);
+printf("The word is:\n");
+switch(num)
+{
+case 1: printf("\n one");
+break;
+case 2: printf("\n two");
+break;
+case 3: printf("\n three");
+break;
+case 4: printf("\n four");
+break;
+case 5: printf("\n five");
+break;
+case 6: printf("\n six");
+break;
+case 7: printf("\n seven");
+break;
+case 8: printf("\n eight");
+break;
+case 9: printf("\n nine");
+break;
+case 10:printf("\n ten");
+break;
+}
+}
