b89449c89fe1890701b7f2ac5199845e80869c7c
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-02-08 16:30:43 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    if(a%7==0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
