29df14a6b61dfe8113f5dd6e9c742328c9c9dac9
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-02-13 15:53:25 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the number: ");
+    scanf("%d",&a);
+    if(a%13==0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
