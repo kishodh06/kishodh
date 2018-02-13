9b4c9617388da647325f5dc0feaaf8e63cac63c6
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-02-13 15:58:44 +0530

    Create sq.c

---
+++ sq.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter the numbers: ");
+    scanf("%d%d",&a,&b);
+    if(a==b)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
