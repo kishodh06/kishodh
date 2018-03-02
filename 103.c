115fbf511cf9677799c08509b9d3d2f0da915de4
kishodh06 <30828267+kishodh06@users.noreply.github.com>
2018-03-02 14:53:34 +0530

    Create 103.c

---
+++ 103.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char name[20];
+	int i;
+	printf("Enter the string:");
+	scanf("%s",name);
+	for(i=0;name[i]!='\0';i++)
+	{
+	if(name[i]>='a'&&name[i]<='z')
+	printf("%c",name[i]-32);
+	else
+	printf("%c",name[i]);
+	}
+}
